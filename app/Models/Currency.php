978389bb5ced770dc1359b38075a39d39912fd33
Ilya Nikiforov <i.nikiforov@uplab.ru>
2022-02-10 22:10:48 +0300

    Add currency synchronizer service

---
+++ app/Models/Currency.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Currency extends Model
+{
+    use HasFactory;
+
+    /**
+     * trend attribute constants
+     */
+    public const UP = 1;
+    public const DOWN = 0;
+    public const EQ = null;
+
+    protected $fillable = [
+        'title',
+        'code',
+        'rate',
+        'base_currency',
+        'crypt',
+        'date',
+        'trend',
+        'trend_diff'
+    ];
+}
--- config/app.php
+++ config/app.php
@@ -178,6 +178,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\RepositoryServiceProvider::class,
         App\Providers\ViewServiceProvider::class,
+        App\Providers\CurrencyServiceProvider::class
 
     ],
 
